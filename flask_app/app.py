543bd2a6cdd1071a3ba6874d1fd641d2df51a8ed
Michele Brunelli <brunelli.mich@gmail.com>
2025-05-14 11:16:10 +0200

    added index.html with bootstrap for pretty layout

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 # create flask app
 app = Flask(__name__)
@@ -6,8 +6,8 @@ app = Flask(__name__)
 # populate flask app
 @app.route("/")
 def index():
-    return jsonify({"msg": "Flask is running!"})
+    return render_template("index.html")
 
 # run flask app on localhost:5000
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True) # debug=True just for develop
