fbf21ea8a4e50132e00502677b2b73b708189053
Michele Brunelli <brunelli.mich@gmail.com>
2025-05-15 18:10:33 +0200

    4st attempt for kafka_to_flask - we have a problem when consume_kafka (consumer) starts

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,10 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from confluent_kafka import Consumer
-import time
+import threading
 
 # create flask app and initialize webSocket
 app = Flask(
@@ -9,50 +12,54 @@ app = Flask(
     static_folder = "static",
     template_folder = "templates"
     )
-socket_io = SocketIO(app, cors_allowed_origins = "*")
+socket_io = SocketIO(app, cors_allowed_origins = "*", async_mode="eventlet")
 
 # Kafka Config
 kafka_config = {
     "bootstrap.servers": "kafka:9092", # kafka container name
-    "group.id": "flask-consumer-group", # consumer group name
+    "group.id": "smart_home", # consumer group name
     "auto.offset.reset": "earliest", # with no offset, start from the beginning
-    "group.id": "smart_home" # group.id name
 }
 
 # Kafka consumer with error handling
 def consume_kafka():
-    time.sleep(10)
-    consumer = Consumer(kafka_config)
-    print(f"Started kafka consumer") # log
-    consumer.subscribe(["smart_home_data"])
-    print(f"Kafka subsribed on topic 'smart_home_data'") #log
+    try:
+        consumer = Consumer(kafka_config)
+        print(f"Started kafka consumer") # log
+        consumer.subscribe(["smart_home_data"])
+        print(f"Kafka subsribed on topic 'smart_home_data'") #log
 
-    while True:
-        msg = consumer.poll(1.0)
-        print(f"Polling...") # log
-        if msg is None:
-            print(f"No message recived") # log
-            continue
-        if msg.error():
-            print(f"[KAFKA] - Error: {msg.error()}") # log
-            continue
-        data = msg.value().decode("utf-8")
-        print(f"[KAFKA] - {data}") # log
-        print(f"[MESSAGE DIMENSION] - {len(data)} bytes") # log
-
-        print(f"📤 Emitting to socket: {data[:80]}...")  # log
-        socket_io.emit('kafka_message', {'data': data})
-        # socket_io.emit('kafka_message', {'data': "test"})
-        print(f"[SocketIO] - Emit: {data}") # log
-    
-    consumer.close()
+        while True:
+            msg = consumer.poll(1.0)
+            print(f"Polling...") # log
+            if msg is None:
+                print(f"No message recived") # log
+                continue
+            if msg.error():
+                print(f"[KAFKA] - Error: {msg.error()}") # log
+                continue
+            data = msg.value().decode("utf-8")
+            print(f"[MESSAGE DIMENSION] - {len(data)} bytes") # log
+            print(f"📤 Emitting to socket: {data[:80]}...")  # log
+            socket_io.emit('kafka_message', {'data': data})
+            # socket_io.emit('kafka_message', {'data': "test"})
+            print(f"[SocketIO] - Emit: {data[:80]}...") # log
+        
+        consumer.close()
+    except Exception as e:
+        import traceback
+        print("❌ EXCEPTION in Kafka thread:")
+        traceback.print_exc()
 
 # set primary route
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return "Flask is running"
 
 # run flask app and kafka consumer on localhost:8000
 if __name__ == "__main__":
-    socket_io.start_background_task(consume_kafka)
+    print(f"Starting FLASK...")
+    socket_io.start_background_task(consume_kafka) # whitout this all works properly
+    # print(f"Started KAFKA THREAD!")
     socket_io.run(app, host="0.0.0.0", port=8000)
