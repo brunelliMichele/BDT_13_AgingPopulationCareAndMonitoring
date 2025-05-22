4127e5a8abfa851f150c34ae98cdced0d60d6fad
Michele Brunelli <brunelli.mich@gmail.com>
2025-05-22 11:50:32 +0200

    bug fixed

--- flask_app/sockets.py
+++ flask_app/sockets.py
@@ -5,6 +5,7 @@ from threading import Thread
 import logging
 from flask_socketio import SocketIO, rooms
 from config import KAFKA_BROKER, SMART_TOPIC, ALERT_TOPIC
+from app import app
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,40 +15,42 @@ def register_sockets(socket_io: SocketIO):
         logging.info("✅ Client connected via WebSocket")
 
     def smart_data_consumer():
-        logging.info("📡 Starting smart_data_consumer thread...")
-        try:
-            consumer = KafkaConsumer(
-                SMART_TOPIC,
-                bootstrap_servers=KAFKA_BROKER,
-                value_deserializer=lambda m: json.loads(m.decode("utf-8")),
-                group_id="smart_data_group"
-            )
+        with app.app_context():
+            logging.info("📡 Starting smart_data_consumer thread...")
+            try:
+                consumer = KafkaConsumer(
+                    SMART_TOPIC,
+                    bootstrap_servers=KAFKA_BROKER,
+                    value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+                    group_id="smart_data_group"
+                )
 
-            for message in consumer:
-                logging.info("🏠 Smart home data received.")
-                logging.info(f"🔍 Emitting... active clients: {rooms(None, namespace='/')}")                
-                socket_io.emit("smart_data_message", message.value, to=None, namespace="/")
+                for message in consumer:
+                    logging.info("🏠 Smart home data received.")
+                    logging.info("🔍 Emitting smart_data_message...")
+                    socket_io.emit("smart_data_message", message.value, to=None, namespace="/")
 
-        except Exception as e:
-            logging.error(f"❌ smart_data_consumer error: {e}")
+            except Exception as e:
+                logging.error(f"❌ smart_data_consumer error: {e}")
 
     def alert_consumer():
-        logging.info("📡 Starting alert_consumer thread...")
-        try:
-            consumer = KafkaConsumer(
-                ALERT_TOPIC,
-                bootstrap_servers=KAFKA_BROKER,
-                value_deserializer=lambda a: json.loads(a.decode("utf-8")),
-                group_id="alert_group"
-            )
+        with app.app_context():
+            logging.info("📡 Starting alert_consumer thread...")
+            try:
+                consumer = KafkaConsumer(
+                    ALERT_TOPIC,
+                    bootstrap_servers=KAFKA_BROKER,
+                    value_deserializer=lambda a: json.loads(a.decode("utf-8")),
+                    group_id="alert_group"
+                )
 
-            for message in consumer:
-                logging.info("⚠️ Alert received and sent.")
-                logging.info(f"🔍 Emitting... active clients: {rooms(None, namespace='/')}")
-                socket_io.emit("new_alert_message", message.value, to=None, namespace="/")
+                for message in consumer:
+                    logging.info("⚠️ Alert received and sent.")
+                    logging.info("🔍 Emitting alert...")
+                    socket_io.emit("new_alert_message", message.value, to=None, namespace="/")
 
-        except Exception as e:
-            logging.error(f"❌ alert_consumer error: {e}")
+            except Exception as e:
+                logging.error(f"❌ alert_consumer error: {e}")
 
     # Avvio thread
     Thread(target=smart_data_consumer, name="SmartDataConsumer", daemon=True).start()
